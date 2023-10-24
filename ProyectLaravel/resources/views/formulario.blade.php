d70185b4267b8a151db15cc9f6c265cc1b30e615
YaritziLeon <yaritzileonm08@gmail.com>
2023-10-23 21:52:57 -0600

    Practica6 Form Request & Mensajes

--- ProyectLaravel/resources/views/formulario.blade.php
+++ ProyectLaravel/resources/views/formulario.blade.php
@@ -4,6 +4,22 @@
     <h1 class="display-1 text-center text-info">FORMULARIO</h1>
 
     <div class='container mt-5 col-md-6'>
+      @if(session()->has('confirmacion'))
+      <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <strong> {{session('confirmacion')}} </strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      @endif
+
+      {{-- @if($errors->any())
+      @foreach ($errors->all() as $error)
+          
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong> {{ $error }} </strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      @endforeach
+      @endif --}}
 
     <div class="card">
         <div class="card-header fs-3 fw-bold text-center">
@@ -14,11 +30,13 @@
               @csrf
                 <div class="mb-3">
                   <label class="form-label">Titulo: </label>
-                  <input type="text" name="txtTitulo" class="form-control">
+                  <input type="text" name="txtTitulo" class="form-control" value="{{old('txtTitulo')}}">
+                  <p class="text-danger fst-italic fw-bold"> {{$errors->first('txtTitulo')}}
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Recuerdos: </label>
-                    <input type="text" name="txtRecuerdos" class="form-control">
+                    <input type="text" name="txtRecuerdos" class="form-control" value="{{old('txtRecuerdos')}}">
+                    <p class="text-danger fst-italic fw-bold">{{$errors->first('txtRecuerdos')}}
                   </div>
         </div>
         <div class="card-footer text-body-secondary text-uppercase">
